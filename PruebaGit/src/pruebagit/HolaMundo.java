85b443cab77f69811c9724457471c37ddc00d435
iñigo calderon <i.calderon@opendeusto.es>
2023-09-27 11:01:14 +0200

    Arreglao

--- PruebaGit/src/pruebagit/HolaMundo.java
+++ PruebaGit/src/pruebagit/HolaMundo.java
@@ -2,11 +2,7 @@ package pruebagit;
 
 public class HolaMundo {
 	public static void main (String[] args) {
-<<<<<<< HEAD
-		
-=======
 		System.out.println("EOOO");
->>>>>>> branch 'master' of https://github.com/InigoCalderon/HolaMundo.git
 	}
 	public int sumar(int a, int b){
 		return a + b;
